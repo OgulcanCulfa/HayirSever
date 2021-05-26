c73d535a56a16324fc2759f9e858b530be0711ec
OgulcanCulfa <ogulcanculfa@gmail.com>
2021-05-26 20:35:37 +0300

    release

--- app.js
+++ app.js
@@ -24,9 +24,7 @@ require("./socket/chat").socket(io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.set("api_key", process.env.API_KEY || "secret");
 app.use(express.static("public"));
--- serve.js
+++ serve.js
@@ -14,8 +14,6 @@ app.use(routers.userRouter);
 app.use(routers.categoryRouter);
 app.use(routers.test);
 
-app.use((req, res, next) => {
-  res.status(404).send("404 NOT FOUND");
-});
+
 
 module.exports = app;
