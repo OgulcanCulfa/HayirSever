544918d28ec30cbbdfcfe922261b1800e1d8dc66
OgulcanCulfa <ogulcanculfa@gmail.com>
2021-05-26 19:52:53 +0300

    before release

--- server/app.js
+++ server/app.js
@@ -5,23 +5,32 @@ const cors = require("cors");
 const serve = require("./serve");
 const logger = require("morgan");
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000"
-  }
-});
+const io = require("socket.io")(
+  server //{
+  //cors: {
+  //origin: "http://localhost:3000",
+  //},
+  //}
+);
 require("./socket/chat").socket(io);
 
-app.use(cors());
-app.use(logger("dev"));
+//app.use(cors());
+
+//app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("api_key", process.env.API_KEY || "secret");
 app.use(express.static("public"));
 app.use(serve);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 server.listen(PORT, () => {
   console.log("Ready on http://localhost:" + PORT);
