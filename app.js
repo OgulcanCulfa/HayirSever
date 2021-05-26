6e55e93a5604fd95e78f9cec43816487767fb27f
OgulcanCulfa <ogulcanculfa@gmail.com>
2021-05-27 00:27:09 +0300

    release

--- app.js
+++ app.js
@@ -26,13 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set("api_key", process.env.API_KEY || "secret");
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + "/client/build")));
 app.use(serve);
 
-app.use(express.static(path.join(__dirname + "/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-
 
 server.listen(process.env.PORT || PORT, () => {
   console.log("Server is ready on port: " + PORT);
